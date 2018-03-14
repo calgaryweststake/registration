13a73d29e243b415e047f8bd8195c615b922ef98
Dave Anderson <david.anderson@robotsandpencils.com>
2018-03-14 17:08:21 -0600

    incremental improvement

--- hello/lib/hello/youth.ex
+++ hello/lib/hello/youth.ex
@@ -7,13 +7,13 @@ defmodule Hello.Youth do
 
   schema "youth" do
     ## Youth Personal
-    # solo selfie\
     field :first_name, :string
     field :last_name, :string
     field :mobile_number, :string
     field :birth_date, :date
     field :gender, :string
     field :ward, :string
+    # field :photo, ???
     
     ## Emergency Contact
     field :emergency_first_name, :string
@@ -47,7 +47,7 @@ defmodule Hello.Youth do
   def changeset(%Youth{} = youth, attrs \\ %{}) do
 
     youth
-    |> cast(attrs, [:first_name, :last_name, :birth_date, :gender, :ward])
+    |> cast(attrs, [:first_name, :last_name, :mobile_number, :birth_date, :gender, :ward])
     |> validate_required([:first_name, :last_name, :birth_date, :gender, :ward])
     |> validate_length(:first_name, min: 2, max: 30)
     |> validate_length(:last_name, min: 2, max: 30)
