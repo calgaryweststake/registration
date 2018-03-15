67aa0d28311ffe94affe2fb5e2f77daac68f8299
Dave Anderson <david.anderson@robotsandpencils.com>
2018-03-14 18:15:04 -0600

    Improve form validation

--- hello/lib/hello/youth.ex
+++ hello/lib/hello/youth.ex
@@ -14,20 +14,20 @@ defmodule Hello.Youth do
     field :gender, :string
     field :ward, :string
     # field :photo, ???
-    
+
     ## Emergency Contact
     field :emergency_first_name, :string
     field :emergency_last_name, :string
     field :emergency_primary_number, :string
     field :emergency_alternate_number, :string
     field :emergency_relationship, :string
-    
+
     ## Medical Information
     field :medical_allergies, :string
     field :medical_limitations, :string
     field :medical_medications, :string
     field :medical_history, :string
-    
+
     ## Permission
     field :permission_first_name, :string
     field :permission_last_name, :string
@@ -36,7 +36,7 @@ defmodule Hello.Youth do
     field :permission_address, :string
     field :permission_city, :string
     field :permission_province, :string
-    
+
     field :permission_participation_agreement, :string
     field :permission_photographic_agreement, :string
 
@@ -47,11 +47,22 @@ defmodule Hello.Youth do
   def changeset(%Youth{} = youth, attrs \\ %{}) do
 
     youth
-    |> cast(attrs, [:first_name, :last_name, :mobile_number, :birth_date, :gender, :ward])
-    |> validate_required([:first_name, :last_name, :birth_date, :gender, :ward])
+    |> cast(attrs, [:first_name, :last_name, :mobile_number, :birth_date, :gender, :ward, :emergency_first_name, :emergency_last_name, :emergency_primary_number, :emergency_alternate_number, :emergency_relationship, :medical_allergies, :medical_limitations, :medical_medications, :medical_history, :permission_first_name, :permission_last_name, :permission_email, :permission_number, :permission_address, :permission_city, :permission_province, :permission_participation_agreement, :permission_photographic_agreement])
+    |> validate_required([:first_name, :last_name, :birth_date, :gender, :ward, :emergency_first_name, :emergency_last_name, :emergency_primary_number, :emergency_relationship, :permission_first_name, :permission_last_name, :permission_number, :permission_address, :permission_city, :permission_province])
     |> validate_length(:first_name, min: 2, max: 30)
     |> validate_length(:last_name, min: 2, max: 30)
+    |> validate_length(:mobile_number, min: 10, max: 30)
     |> validate_length(:gender, min: 3, max: 30)
     |> validate_length(:ward, min: 3, max: 30)
+    |> validate_length(:emergency_first_name, min: 2, max: 30)
+    |> validate_length(:emergency_last_name, min: 2, max: 30)
+    |> validate_length(:emergency_primary_number, min: 10, max: 30)
+    |> validate_length(:emergency_relationship, min: 3, max: 30)
+    |> validate_length(:permission_first_name, min: 2, max: 30)
+    |> validate_length(:permission_last_name, min: 2, max: 30)
+    |> validate_length(:permission_number, min: 10, max: 30)
+    |> validate_length(:permission_address, min: 2, max: 60)
+    |> validate_length(:permission_city, min: 2, max: 30)
+    |> validate_length(:permission_province, min: 2, max: 30)
   end
 end
