076508952db7fa8fbd801bf9daa1c80c0e3f3222
Dave Anderson <david.anderson@robotsandpencils.com>
2018-03-17 18:40:14 -0600

    abbreviate potentially long medical information in the summary

--- hello/lib/hello_web/views/youth_view.ex
+++ hello/lib/hello_web/views/youth_view.ex
@@ -6,4 +6,49 @@ defmodule HelloWeb.YouthView do
     [first_name, last_name]
     |> Enum.join(" ")
   end
+
+  def abbreviated_allergies(%Youth{medical_allergies: medical_allergies}) do
+    cond do
+      medical_allergies == nil ->
+        ""
+      true ->
+        medical_allergies
+        |> String.slice(0..29)
+        |> Kernel.<>("…")
+    end
+  end
+
+  def abbreviated_limitations(%Youth{medical_limitations: medical_limitations}) do
+    cond do
+      medical_limitations == nil ->
+        ""
+      true ->
+        medical_limitations
+        |> String.slice(0..29)
+        |> Kernel.<>("…")
+    end
+  end
+
+  def abbreviated_medications(%Youth{medical_medications: medical_medications}) do
+    cond do
+      medical_medications == nil ->
+        ""
+      true ->
+        medical_medications
+        |> String.slice(0..29)
+        |> Kernel.<>("…")
+      end
+  end
+
+  def abbreviated_history(%Youth{medical_history: medical_history}) do
+    cond do
+      medical_history == nil ->
+        ""
+      true ->
+        medical_history
+        |> String.slice(0..29)
+        |> Kernel.<>("…")
+    end
+  end
+
 end
