e57b7602667f529bf143d70315f8b75ce22e3af2
Dave Anderson <david.anderson@robotsandpencils.com>
2018-03-30 16:51:32 -0600

    Improve message when youth is created

--- hello/lib/hello_web/controllers/youth_controller.ex
+++ hello/lib/hello_web/controllers/youth_controller.ex
@@ -2,29 +2,29 @@ defmodule HelloWeb.YouthController do
   use HelloWeb, :controller
   alias Hello.Repo
   alias Hello.Youth
-  
+
   def index(conn, _params) do
     youth = Repo.all(Youth)
     render conn, "index.html", youth: youth
   end
-  
+
   def show(conn, %{"id" => id}) do
     youth = Repo.get(Youth, id)
     render conn, "show.html", youth: youth
   end
-  
+
   def new(conn, _params) do
     changeset = Youth.changeset(%Youth{})
     render conn, "new.html", changeset: changeset
   end
-  
+
   def create(conn, %{"youth" => youth_params}) do
     changeset = Youth.changeset(%Youth{}, youth_params)
     case Repo.insert(changeset) do
-      {:ok, youth} ->    
+      {:ok, youth} ->
         conn
-        |> put_flash(:info, "#{youth.first_name} #{youth.last_name} created!")
-        |> redirect(to: youth_path(conn, :index))
+        |> put_flash(:info, "The registration for #{youth.first_name} #{youth.last_name} has been successfully submitted!")
+        |> redirect(to: youth_path(conn, :new))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
