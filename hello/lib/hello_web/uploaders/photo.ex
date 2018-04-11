aa56f45e9341f85acb9657d36d0dc1965709ef16
Dave Anderson <david.anderson@robotsandpencils.com>
2018-04-11 13:37:20 -0600

    compare file extensions lowercased

--- hello/lib/hello_web/uploaders/photo.ex
+++ hello/lib/hello_web/uploaders/photo.ex
@@ -9,7 +9,7 @@ defmodule Hello.Photo do
 
   # Whitelist file extensions:
   def validate({file, _}) do
-    ~w(.jpg .jpeg .gif .png) |> Enum.member?(Path.extname(file.file_name))
+    ~w(.jpg .jpeg .gif .png) |> Enum.member?(String.downcase(Path.extname(file.file_name)))
   end
 
   def storage_dir(_, {_, _}), do: "uploads"
